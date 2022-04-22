e43898b1f5322a79ef3a762bdf043fd60eb317f3
Christoph <c.wagner.germany@gmail.com>
2022-04-22 19:25:42 +0200

    2nd commit

---
+++ models/chicken.class.js
@@ -0,0 +1,13 @@
+class Chicken extends MovableObject {
+
+    constructor() {
+        super().loadImage('img/3.Secuencias_Enemy_básico/Versión_Gallinita (estas salen por orden de la gallina gigantona)/1.Ga_paso_derecho.png');
+
+        this.x = 300 + Math.random() * 300; // Immer Zahl zwischen 300 und 600
+    }
+
+
+    chickenSound() {
+
+    }
+}
---
+++ models/movable-object.class.js
@@ -0,0 +1,21 @@
+class MovableObject {
+    x = 120;
+    y = 250;
+    img;
+    height = 150;
+    width = 100;
+
+    loadImage(path) {
+        // Image() muss nicht definiert werden, da es von JS von
+        // Haus aus zur Verfügung gestellt wird.
+        this.img = new Image();
+        this.img.src = path;
+    }
+
+    // Hier wird auf "function" vor "moveUp()" bewusst verzichtet,
+    // da es sich bei einer class um etwas relativ Modernes handelt.
+    // In moderneren Elementen kann darauf verzichtet werden. 
+    moveRight() {
+        console.log('Moving up');
+    }
+}
