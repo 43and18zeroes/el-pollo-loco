027d6b29b0f5bad95ded9dbd82d8fd22f60c38fd
Christoph <c.wagner.germany@gmail.com>
2022-04-26 22:20:51 +0200

    abjusted y-axis

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -1,4 +1,7 @@
 class Chicken extends MovableObject {
+    // Die y Achse, welche für alle in movable-objects.class.js festgelegt wurde,
+    // wird hier überschrieben, damit die chicken etwas besser ins Bild passen
+    y = 290;
 
     constructor() {
         super().loadImage('img/3.Secuencias_Enemy_básico/Versión_Gallinita (estas salen por orden de la gallina gigantona)/1.Ga_paso_derecho.png');
--- models/world.class.js
+++ models/world.class.js
@@ -11,7 +11,9 @@ class World {
         new Cloud()
     ];
     backgroundObjects = [
-        new BackgroundObject('img/5.Fondo/Capas/3.Fondo3/1.png', 0)
+        new BackgroundObject('img/5.Fondo/Capas/3.Fondo3/1.png', 0),
+        new BackgroundObject('img/5.Fondo/Capas/2.Fondo2/1.png', 0),
+        new BackgroundObject('img/5.Fondo/Capas/1.suelo-fondo1/1.png', 0)
     ];
 
     // Hilfsvariable canvas, da clearRect ansonsten nicht auf
