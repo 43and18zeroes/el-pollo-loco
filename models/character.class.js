7d805fc0d11c0af49a24d7475073c40dda4f27b1
Christoph <c.wagner.germany@gmail.com>
2022-04-26 22:46:32 +0200

    adjusted character height

--- models/character.class.js
+++ models/character.class.js
@@ -1,5 +1,10 @@
 class Character extends MovableObject {
 
+    width = 140;
+    height = 280;
+
+    y = 155;
+
     constructor() {
         super().loadImage('img/2.Secuencias_Personaje-Pepe-corrección/2.Secuencia_caminata/W-21.png');
     }
