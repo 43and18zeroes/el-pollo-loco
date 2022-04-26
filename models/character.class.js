516b1746ada30b591857c6daece16e208c31c59a
Christoph <c.wagner.germany@gmail.com>
2022-04-27 00:04:50 +0200

    added movement to character

--- models/character.class.js
+++ models/character.class.js
@@ -1,16 +1,35 @@
 class Character extends MovableObject {
 
-    width = 140;
-    height = 280;
+    width = 125;
+    height = 250;
+    y = 180;
 
-    y = 155;
+    IMAGES_WALKING = [
+        'img/2.Secuencias_Personaje-Pepe-corrección/2.Secuencia_caminata/W-21.png',
+        'img/2.Secuencias_Personaje-Pepe-corrección/2.Secuencia_caminata/W-22.png',
+        'img/2.Secuencias_Personaje-Pepe-corrección/2.Secuencia_caminata/W-23.png',
+        'img/2.Secuencias_Personaje-Pepe-corrección/2.Secuencia_caminata/W-24.png',
+        'img/2.Secuencias_Personaje-Pepe-corrección/2.Secuencia_caminata/W-25.png',
+        'img/2.Secuencias_Personaje-Pepe-corrección/2.Secuencia_caminata/W-26.png'
+    ];
+    currentImage = 0;
 
     constructor() {
         super().loadImage('img/2.Secuencias_Personaje-Pepe-corrección/2.Secuencia_caminata/W-21.png');
+        this.loadImages(this.IMAGES_WALKING),
+
+        this.animate();
     }
 
-    moveRight() {
-
+    animate() {
+        setInterval(() => {
+            // erreicht this.currentImage den Wert 6 wird i durch die Modulo Rechnung
+            // auf 0 gesetzt: Selbsterstellte Endlosschleife
+            let i = this.currentImage % this.IMAGES_WALKING.length;
+            let path = this.IMAGES_WALKING[i];
+            this.img = this.imageCache[path];
+            this.currentImage++;
+        }, 100);
     }
 
     jump() {
--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -4,6 +4,7 @@ class MovableObject {
     img;
     height = 150;
     width = 100;
+    imageCache = {};
 
     loadImage(path) {
         // Image() muss nicht definiert werden, da es von JS von
@@ -12,9 +13,25 @@ class MovableObject {
         this.img.src = path;
     }
 
+    /**
+     * 
+     * @param {Array} arr - ['img/image1.png', 'img/image2.png', ...] 
+     */
+    loadImages(arr) {
+        arr.forEach((path) => {
+            let img = new Image();
+            img.src = path;
+            this.imageCache[path] = img;
+        });
+    }
+
     // Hier wird auf "function" vor "moveUp()" bewusst verzichtet,
     // da es sich bei einer class um etwas relativ Modernes handelt.
     // In moderneren Elementen kann darauf verzichtet werden. 
+    moveRight() {
+        console.log('Moving right');
+    }
+
     moveLeft() {
 
     }
