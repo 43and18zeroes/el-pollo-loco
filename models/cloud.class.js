5d10634cd186dd5bd252aece41fb1c99ec2085a4
Christoph <c.wagner.germany@gmail.com>
2022-04-26 23:08:32 +0200

    cloud movement

--- models/cloud.class.js
+++ models/cloud.class.js
@@ -7,6 +7,15 @@ class Cloud extends MovableObject {
         super().loadImage('img/5.Fondo/Capas/4.nubes/1.png');
 
         this.x = Math.random() * 500; // Immer Zahl zwischen 300 und 600
+        // Funktionsaufruf zur Bewegung der Wolken
+        this.animate();
+    }
+
+    // Wolken werden mit 60fps bewegt
+    animate() {
+        setInterval(() => {
+            this.x -= 0.15;
+        }, 1000 / 60);
     }
 
 
